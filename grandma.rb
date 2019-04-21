b6e065fa183e5b55313e47336713816500a7c2b1
Ryan Gilfillan <regilfillan@gmail.com>
2019-04-21 15:41:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 def speak_to_grandma (phrase)
   if phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else
     "NO, NOT SINCE 1938!"
-    
+  end
 end
